5d62058c4b3e4c19f2e8bf3cc51ade1e9f9de2eb
chris88herr <chris88herrera@gmail.com>
2020-11-03 16:48:37 -0800

    changed the way activities passed data arround

---
+++ app/src/main/java/com/example/note/FirstFragment.java
@@ -0,0 +1,31 @@
+package com.example.note;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.widget.Spinner;
+
+import androidx.navigation.fragment.NavHostFragment;
+
+public class FirstFragment extends Fragment {
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first, container, false);
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+    }
+}
